69bb1be2b049c210162250ab81f7d0c60859c609
Swoosky <agushusky@gmail.com>
2021-12-07 21:49:01 +0800

    done create

--- src/components/Articles/create.vue
+++ src/components/Articles/create.vue
@@ -1,41 +1,126 @@
 <template>
     <v-main>
         <h1 class="black--text text-h4 font-weight-bold mb-1 text-left">Create Article</h1>
-        <v-form v-model="valid">
+        <v-form v-model="valid" ref="form">
             <v-card elevation="5" class="weight ml-15 mr-15 mt-10">
                 <v-row>
                     <v-col cols="12" md="6" class="ml-10">
                         <h3 class="font-weight-medium text-left">Title</h3>
                         <v-text-field
-                        v-model="title"
+                        v-model="form.title"
                         label="Article Title (Required)"
                         counter="100"
                         required></v-text-field>
                     </v-col>
 
-                    <v-col cols="12" md="5" class="mr-10">
+                    <v-col cols="12" md="6" class="ml-10">
                         <h3 class="font-weight-medium text-left">Image URL</h3>
                         <v-text-field
-                        v-model="img_url"
+                        v-model="form.img_url"
                         label="Image URL (Required)"
                         counter="100"
                         required></v-text-field>
                     </v-col>
 
-                    <v-col cols="12" md="11" class="ml-10">
+                    <v-col cols="12" md="6" class="ml-10">
                         <h3 class="font-weight-medium text-left">Body</h3>
-                        <v-text-field
-                        v-model="body"
+                        <v-textarea
+                        v-model="form.body"
                         label="Article Text (Required)"
-                        counter="1000"
-                        required></v-text-field>
+                        auto-grow
+                        rows="15"
+                        required></v-textarea>
                     </v-col>
 
-                    <v-col class="ml-9">
-                        <v-btn color="blue darken-1">Submit</v-btn>
+                    <v-col cols="12" md="6" class="ml-10">
+                        <v-btn color="blue darken-1" @click="save">Submit</v-btn>
                     </v-col>
                 </v-row>
             </v-card>
         </v-form>
+        <v-snackbar v-model="snackbar" :color="color" timeout="2000" bottom> {{ error_message }}</v-snackbar>
     </v-main>
 </template>
+
+<script>
+export default {
+    name: "List",
+    data() {
+        return {
+            load: false,
+            snackbar: false,
+            error_message: '',
+            valid: false,
+            color: '',
+            headers: [],
+            article: new FormData,
+            articles: [],
+            form: {
+                img_url:'',
+                title:'',
+                body:'',
+                author:''
+            }
+        };
+    },
+    methods: {
+        // Simpan data article
+        save() {
+            this.article.append('img_url', this.form.img_url);
+            this.article.append('title', this.form.title);
+            this.article.append('body', this.form.body);
+            this.article.append('author', localStorage.getItem('au_first_name') + ' ' + localStorage.getItem('au_last_name'));
+
+            var url = this.$api + '/article';
+            this.load = true;
+            this.$http.post(url, this.article, {
+                headers: {
+                    'Authorization' : 'Bearer ' + localStorage.getItem('token'),
+                },
+            }).then(response => {
+                this.error_message = response.data.message;
+                this.color = "green";
+                this.snackbar = true;
+                this.load = true;
+                this.$router.push({
+                    name: 'Home',
+                });
+            }).catch(error => {
+                this.error_message = error.response.data.message;
+                this.color = "red";
+                this.snackbar = true;
+                this.load = false;
+            });
+        },
+        
+        close() {
+            this.dialog = false;
+            this.inputType = 'Tambah';
+            this.dialogConfirm = false;
+            this.readData();
+        },
+
+        cancel() {
+            this.resetForm();
+            this.readData();
+            this.dialog = false;
+            this.dialogConfirm = false;
+            this.inputType = 'Tambah';
+        },
+
+        resetForm() {
+            this.form = {
+                nama_kelas: null,
+                kode: null,
+                biaya_pendaftaran: null,
+                kapasitas: null,
+            };
+        },
+    },
+    computed: {
+        formTitle() {
+            return this.inputType;
+        },
+    },
+};
+</script>
