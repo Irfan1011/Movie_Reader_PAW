aaaf208489158775621906680ea5859293316446
Swoosky <agushusky@gmail.com>
2021-12-07 15:57:39 +0800

    Change api link to heroku

--- src/main.js
+++ src/main.js
@@ -7,7 +7,7 @@ import axios from 'axios';
 Vue.config.productionTip = false
 
 Vue.prototype.$http = axios; // code standar
-Vue.prototype.$api = 'http://127.0.0.1:8000.api' // link backend
+Vue.prototype.$api = 'https://api-moviereader.herokuapp.com/api' // link backend
 
 new Vue({
   vuetify,
